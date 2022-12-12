c28fc90142d01bf1662ad99d9f915bb09686b898
empergar <eperezga.inf@upsa.es>
2022-12-12 09:05:28 +0100

    Dia 4

--- src/main/java/es/upsa/sbd2/CsvReader.java
+++ src/main/java/es/upsa/sbd2/CsvReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +13,12 @@ public class CsvReader {
     public <T> List<T> read(File csvFile, CsvParser<T> csvParser) throws IOException {
 
         List<T> list = new ArrayList<>();
-        try (FileReader fr = new FileReader(csvFile);
+        try (FileReader fr = new FileReader(csvFile, Charset.forName("UTF-8"));
              BufferedReader br = new BufferedReader(fr)){
 
             String csvLine = br.readLine();
 
+            csvLine = br.readLine();
             //Hasta la ultima linea
             while ( csvLine != null ){
 
