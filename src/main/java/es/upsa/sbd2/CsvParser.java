18b7f3a3594d940ef0981e3162c8e129d1808481
Pablo <pmartinsa.inf@upsa.es>
2022-12-20 01:44:14 +0100

    Dia 3

--- src/main/java/es/upsa/sbd2/CsvParser.java
+++ src/main/java/es/upsa/sbd2/CsvParser.java
@@ -1,14 +1,21 @@
 package es.upsa.sbd2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface CsvParser <T>{
 
     T parse(String csvLine);
 
-    List<T> getListaTelefonos(String tlf1, String tlf2, String tlf3)
+    default List<String> getListaTelefonos(List<String> tlfs)
     {
-
+        List<String> telefonos = new ArrayList<>();
+        tlfs.forEach( tlf -> {
+            if (!tlf.isEmpty()) {
+                telefonos.add(tlf);
+            }
+        });
+        return telefonos;
     }
 
 }
